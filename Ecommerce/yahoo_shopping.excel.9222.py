b3eeedb084b029f978e3b04474a4473e412a29f0
Tai <tiknzk@gmail.com>
2018-10-15 22:20:48 +0700

    Update Ecommerce/yahoo_shopping.excel.9222.py

--- Ecommerce/yahoo_shopping.excel.9222.py
+++ Ecommerce/yahoo_shopping.excel.9222.py
@@ -15,6 +15,10 @@ chrome_driver = "/usr/local/bin/chromedriver"
 driver = webdriver.Chrome(chrome_driver, chrome_options=chrome_options)
 
 i=1
+
+row = 2
+row1 =2
+
 path = ''
 
 file_name = "yahoo.xlsx"
@@ -30,35 +34,42 @@ ws.cell(row=1, column=5).value = 'URL'
 ws.cell(row=1, column=6).value = 'Price'
 
 page = 1
+row = 2
+row1 = 2
+
 url= 'https://shopping.yahoo.co.jp/category/2498/list?oq=&uIv=on&pf=&pt=&used=0&seller=0&ei=UTF-8&b=' + str(page)
 driver.get(url)
 
-soup = BeautifulSoup(driver.page_source, 'lxml')
-row = 2
-row1 =2
-a = datetime.now()
-for parent in soup.find_all(class_='mdSearchList elList'):
-    for item in parent.find_all('dd', class_="elName", string=True):
-        for link in item.find_all('a'):
-            ws.cell(row=row, column=4).value = item.string
-            ws.cell(row=row, column=5).value = link.get('href')
-            ws.cell(row=row, column=1).value = a.ctime()
-            ws.cell(row=row, column=2).value = soup.head.title.text
-            ws.cell(row=row, column=3).value = soup.find(class_="elOrder", string=True)
-            row +=1
+while True:
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    a = datetime.now()
+    for parent in soup.find_all(class_='mdSearchList elList'):
+        for item in parent.find_all('dd', class_="elName", string=True):
+            for link in item.find_all('a'):
+                ws.cell(row=row, column=4).value = item.string
+                ws.cell(row=row, column=5).value = link.get('href')
+                ws.cell(row=row, column=1).value = a.ctime()
+                ws.cell(row=row, column=2).value = soup.head.title.string
+                ws.cell(row=row, column=3).value = soup.find(class_="elOrder", string=True)
+                row +=1
 
-for pricea in soup.find_all(class_='elUnit', text=re.compile('円')):
-    for priceb in pricea.previous_sibling:
-        print(priceb)
-        ws.cell(row=row, column=6).value = priceb
-        row1 +=1
+    for pricea in soup.find_all(class_='mdSearchList elList'):
+        for priceb in pricea.find_all(class_="elUnit"):
+            for pricec in priceb.find_previous_siblings('span'):
+                ws.cell(row=row1, column=6).value = pricec.text
+                row1 +=1
+
+    page +=1
+    print(page)
+    wb.save(path + file_name)
+    next = driver.find_element_by_xpath('//*[@class="elNext"]')
+    ActionChains(driver).move_to_element(next).click().perform()
+    print(row1)
 
 
-wb.save(path + file_name)
-print(row)
-print ("")
-next = driver.find_element_by_xpath('//*[@class="elNext"]')
-ActionChains(driver).move_to_element(next).click().perform()
+
+
+
 
 
 
