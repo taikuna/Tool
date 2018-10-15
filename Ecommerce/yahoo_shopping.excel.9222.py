da223a1c60ae0ae96e1181d9ae2f3618d80ccebd
Tai <tiknzk@gmail.com>
2018-10-15 23:52:08 +0700

    Update Ecommerce/yahoo_shopping.excel.9222.py

--- Ecommerce/yahoo_shopping.excel.9222.py
+++ Ecommerce/yahoo_shopping.excel.9222.py
@@ -32,10 +32,14 @@ ws.cell(row=1, column=3).value = 'Hit'
 ws.cell(row=1, column=4).value = 'Item'
 ws.cell(row=1, column=5).value = 'URL'
 ws.cell(row=1, column=6).value = 'Price'
+ws.cell(row=1, column=7).value = 'Seller'
+
 
 page = 1
 row = 2
 row1 = 2
+row2 = 2
+row3 =2
 
 url= 'https://shopping.yahoo.co.jp/category/2498/list?oq=&uIv=on&pf=&pt=&used=0&seller=0&ei=UTF-8&b=' + str(page)
 driver.get(url)
@@ -43,28 +47,41 @@ driver.get(url)
 while True:
     soup = BeautifulSoup(driver.page_source, 'lxml')
     a = datetime.now()
+    title = soup.head.title
+    for pseller in soup.find_all(class_="elSeller"):
+        for seller in pseller.find_next_siblings('span'):
+                ws.cell(row=row2, column=7).value = seller.text
+                row2 +=1
+
     for parent in soup.find_all(class_='mdSearchList elList'):
         for item in parent.find_all('dd', class_="elName", string=True):
             for link in item.find_all('a'):
                 ws.cell(row=row, column=4).value = item.string
                 ws.cell(row=row, column=5).value = link.get('href')
                 ws.cell(row=row, column=1).value = a.ctime()
-                ws.cell(row=row, column=2).value = soup.head.title.string
-                ws.cell(row=row, column=3).value = soup.find(class_="elOrder", string=True)
+                ws.cell(row=row, column=2).value = title.text
+                ws.cell(row=row, column=3).value = 'page'+str(page)
                 row +=1
 
     for pricea in soup.find_all(class_='mdSearchList elList'):
         for priceb in pricea.find_all(class_="elUnit"):
-            for pricec in priceb.find_previous_siblings('span'):
-                ws.cell(row=row1, column=6).value = pricec.text
+            for price in priceb.find_previous_siblings('span'):
+                ws.cell(row=row1, column=6).value = price.text
+
                 row1 +=1
 
     page +=1
-    print(page)
+    print(title.text)
     wb.save(path + file_name)
     next = driver.find_element_by_xpath('//*[@class="elNext"]')
     ActionChains(driver).move_to_element(next).click().perform()
-    print(row1)
+    print('page'+ str(page))
+
+    stop = 15
+
+    if page == stop:
+        break
+
 
 
 
